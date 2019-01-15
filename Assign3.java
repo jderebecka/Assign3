8ec1023107fd0cf8a5a24e169f31d5fad052d7f6
PAYNEA03 <46678732+PAYNEA03@users.noreply.github.com>
2019-01-15 16:37:33 +0000

    finished tuesday
    stopping at red

--- Assign3.java
+++ Assign3.java
@@ -2,22 +2,29 @@ import ShefRobot.*;
 
 public class Assign3 {
 
-    public static ColorSensor.Color color = ColorSensor.Color.BLACK;
-    public static ColorSensor.Color col = sensor.getColor();
-
     public static Robot myRobot = new Robot("dia-lego-e5");
+
+    //public static ColorSensor.Color color = ColorSensor.Color.BLACK;
+
 
     public static ColorSensor sensor =
       myRobot.getColorSensor(Sensor.Port.S1);
     public static GyroSensor angle =
       myRobot.getGyroSensor(Sensor.Port.S2);
+    //public static String color =
+      //String.valueOf(sensor.getColor());
     public static Motor leftMotor =
       myRobot.getLargeMotor(Motor.Port.A);
     public static Motor rightMotor =
       myRobot.getLargeMotor(Motor.Port.D);
+    public static Motor grapple =
+      myRobot.getMediumMotor(Motor.Port.C);
     public static Speaker mySpeaker = myRobot.getSpeaker();
 
-    public static boolean store = false;
+
+    public static boolean ball = false;
+    final static int SPEED = 200;
+    final static int WAIT = 100;
 
     public static void forward (int speed, int wait) {
       leftMotor.setSpeed(speed);
@@ -46,51 +53,103 @@ public class Assign3 {
       System.out.println(sensor.getColor());
     }
 
+    public static void pivotRight (int speed, int wait) {
+      rightMotor.setSpeed(100);
+      rightMotor.backward();
+      leftMotor.setSpeed(100);
+      leftMotor.forward();
+      myRobot.sleep(wait);
+      System.out.println(sensor.getColor());
+    }
+
+    public static void pivotLeft (int speed, int wait) {
+      rightMotor.setSpeed(100);
+      rightMotor.forward();
+      leftMotor.setSpeed(100);
+      leftMotor.backward();
+      myRobot.sleep(wait);
+      System.out.println(sensor.getColor());
+    }
+
     public static void rotate (int rotation) {
       angle.reset();
       angle.getAngle();
       leftMotor.rotate(rotation);
     }
 
-    public static void lineCheck (String colorsearch, int, angle int speed, int wait) {
-      rotate(angle);
+    public static void lineCheck (String find, int speed, int wait) {
+      if (ball==false) {
+        //rotate(-60);
+        rotate(-45);
+      } else {
+        rotate(45);
+      }
       do {
-      rightTurn(speed,wait);
-      } while (String.valueOf(sensor.getColor()) != colorsearch);
+        if (ball==false) {
+          pivotRight(speed,wait);
+        } else {
+          pivotLeft(speed,wait);
+        }
+      } while (String.valueOf(sensor.getColor()) != find);
+    }
+
+    public static void openGrab (int speed, int wait) {
+      grapple.setSpeed(speed);
+      grapple.forward();
+      myRobot.sleep(wait);
+    }
+
+    public static void closeGrab (int speed, int wait) {
+      grapple.setSpeed(speed);
+      grapple.backward();
+      myRobot.sleep(wait);
     }
 
     public static void playSound1 (int tone, int pitch) {
       mySpeaker.playTone(tone,pitch);
     }
 
+
+
+//-------------------------------------------------------------------------//
 	  public static void main(String[] args) {
 
-		  //col = sensor.getColor();
-		  //color = ColorSensor.Color.BLACK;
+
+
+    //closeGrab(500,3000);
+  //  openGrab(500,3000);
+
+
 
 //Start position - get to black line
       do {
-        forward(100,100);
+        forward(SPEED,WAIT);
       } while (String.valueOf(sensor.getColor()) != "BLACK");
-
 
 
 //reached black line, rotate to start following line
       rotate(-90);
 
 
-
 //move along line
       do {
-        lineCheck("BLACK",-45,250,400);
+        lineCheck("BLACK",SPEED,WAIT);
         do {
-          forward(100,100);
+          forward(SPEED,WAIT);
         } while (String.valueOf(sensor.getColor()) == "BLACK");
     } while (String.valueOf(sensor.getColor()) != "RED");
-
+    if (String.valueOf(sensor.getColor()) == "RED"){
 //ONCE RED, OPEN PINCERS
-//MOVE FORWARD TO COLLECT BALL
 //CLOSE PINCERS
+    ball==true;
+    }
+      do {
+        lineCheck("BLACK",SPEED,WAIT);
+        do {
+          forward(SPEED,WAIT);
+        } while (String.valueOf(sensor.getColor()) == "BLACK");
+      } while (String.valueOf(sensor.getColor()) != "YELLOW");
+
 //ROTATE 180DEGREES
 //MOVE FORWARD UNTIL NOT RED
 //IF BLACK, FOLLOW LINE, IF WHITE, FIND LINE
